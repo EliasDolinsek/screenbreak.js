7d24a2456249385eb21692941d3e00d7b4d89257
Elias Dolinsek <elias.dolinsek@gmail.com>
2020-11-21 15:36:27 +0100

    Initial Countdown

---
+++ src/components/Countdown.vue
@@ -0,0 +1,52 @@
+<template>
+  <div>
+    <h1>{{ remainingTime }}</h1>
+    <button @click="pauseContinueCountdown()">PAUSE/CONTINUE</button>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Countdown",
+  data() {
+    return {
+      workTime: 10,
+      breakTime: 10,
+      remainingTime: 10,
+      paused: false,
+      mode: "work", //work, break
+    };
+  },
+  mounted() {
+    this.countdown();
+  },
+  methods: {
+    countdown() {
+      if (!this.paused) {
+        this.remainingTime -= 1;
+      }
+
+      if (this.remainingTime < 0) {
+        this.changeMode()
+      }
+
+      setTimeout(this.countdown, 1000);
+    },
+    pauseContinueCountdown() {
+      this.paused = !this.paused;
+    },
+    changeMode() {
+      if (this.mode == "work") {
+        this.mode = "break"
+        this.remainingTime = this.breakTime
+      } else {
+        this.mode = "work"
+        this.remainingTime = this.workTime
+      }
+    },
+  },
+};
+</script>
+
+<style scoped>
+</style>
