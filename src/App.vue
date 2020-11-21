e1f5b342a3dfe18a38ed00717bebf7c548b72f6f
Elias Dolinsek <elias.dolinsek@gmail.com>
2020-11-21 16:14:56 +0100

    Updated countdown

--- src/App.vue
+++ src/App.vue
@@ -1,23 +1,75 @@
 <template>
-  <div id="app">
-    <Countdown />
+  <div v-bind:class="{ work: working }">
+    <h1 class="text-center">{{ remainingTime }}</h1>
+    <div v-if="working">
+      <p>until next break</p>
+    </div>
+    <div v-else>
+      <p>until break ends</p>
+    </div>
+    <b-row>
+      <b-icon icon="pause-fill"></b-icon>
+    </b-row>
   </div>
 </template>
 
 <script>
-import Countdown from './components/Countdown.vue'
-
 export default {
-  name: 'App',
-  components: {
-    Countdown
+  name: "Countdown",
+  data() {
+    return {
+      workTime: 10,
+      breakTime: 10,
+      remainingTime: 10,
+      paused: false,
+      working: true,
+    };
   },
-}
+  mounted() {
+    this.countdown();
+  },
+  methods: {
+    countdown() {
+      if (!this.paused) {
+        this.remainingTime -= 1;
+      }
+
+      if (this.remainingTime < 0) {
+        this.changeMode();
+      }
+
+      setTimeout(this.countdown, 1000);
+    },
+    pauseContinueCountdown() {
+      this.paused = !this.paused;
+    },
+    changeMode() {
+      this.working = !this.working;
+      if (this.working) {
+        this.remainingTime = this.workTime;
+      } else {
+        this.remainingTime = this.breakTime;
+      }
+    },
+  },
+};
 </script>
 
+<style scoped>
+.work {
+  background-color: #5998c5;
+}
+</style>
+
 <style>
+@import url("https://fonts.googleapis.com/css2?family=Open+Sans:wght@400;600;700&display=swap");
+
+* {
+  margin: 0;
+}
+
 #app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
+  font-family: "Open Sans", sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
